93bd9e4b641a0e17b7c2d1ab4d1843eda025aeb1
louaysaidi22 <louaysaidi18@gmail.com>
2021-12-21 22:03:16 +0100

    task11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if b == 0:
+        return 1
+    if b > 0:
+        c = 1
+        d = a
+        while c < b:
+            d *= a
+            c += 1
+        return d
+    else:
+        c = 1
+        d = 1
+        while c <= -b:
+            d /= a
+            c += 1
+        return d
