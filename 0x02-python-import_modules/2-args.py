74804332712da68346d9744af95de96766d33a95
louaysaidi22 <louaysaidi18@gmail.com>
2021-12-23 19:46:39 +0100

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-p = len(argv)
-if p == 1:
-    print("0 arguments.")
-elif p == 2:
-    print("1 argument:")
-    print("1:", argv[1])
-else:
-    print("{:d} arguments:".format(p - 1))
-    for i in range(1, p):
-        print("{:d}: ".format(i), argv[i])
+if __name__ == "__main__":
+    from sys import argv
+    p = len(argv)
+    if p == 1:
+        print("0 arguments.")
+    elif p == 2:
+        print("1 argument:")
+        print("1:", argv[1])
+    else:
+        print("{:d} arguments:".format(p - 1))
+        for i in range(1, p):
+            print("{:d}: ".format(i), argv[i])
