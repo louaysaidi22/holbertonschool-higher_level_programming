ed7fd6d8b5d83d92713d20fe14f0ad6790373d6e
louaysaidi22 <louaysaidi18@gmail.com>
2021-12-23 19:55:13 +0100

    task3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    p = len(argv)
+    if p == 1:
+        print("0 ")
+    elif p == 2:
+        print(argv[1])
+    else:
+        s = 0
+        for i in range(1, p):
+            s += int(argv[i])
+        print("{:d}".format(s))
