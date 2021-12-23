a2e6385049e5d062a5f4efe8b05b011ec4263d63
louaysaidi22 <louaysaidi18@gmail.com>
2021-12-23 19:46:00 +0100

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+p = len(argv)
+if p == 1:
+    print("0 arguments.")
+elif p == 2:
+    print("1 argument:")
+    print("1:", argv[1])
+else:
+    print("{:d} arguments:".format(p - 1))
+    for i in range(1, p):
+        print("{:d}: ".format(i), argv[i])
