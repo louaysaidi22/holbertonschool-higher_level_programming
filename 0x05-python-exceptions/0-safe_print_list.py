36700f1411c0a09566a18b458db8f8d33c616175
root <root@localhost.localdomain>
2022-01-17 18:01:18 +0000

    task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    l = 0
-    for i range(0, x):
+    ll = 0
+    for i in range(0, x):
         try:
-
-            print("{}".format(my_list[i], end ="")
-            l = l + 1
-        except IndexError: 
+            print("{}".format(my_list[i]), end="")
+            ll += 1
+        except IndexError:
             break
-    return l    
+    print("")
+    return ll
