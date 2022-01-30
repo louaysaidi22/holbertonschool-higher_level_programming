52a60ed9cb9a4de92f7d0e6950f46499d501bea3
louaysaidi22 <louaysaidi18@gmail.com>
2022-01-30 16:05:21 +0100

    task3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""task3"""
+
+
+def print_square(size):
+    """function that prints a square with the character #"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        [print("#", end = "") for j in range(size)]
+        print("")
